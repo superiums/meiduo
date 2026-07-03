2cf500954ea9b00a687203f331d0eb6738b46c21
santo <dadawin@yandex.com>
2026-07-03 19:57:34 +0800

    fix import

--- app/src/main/java/com/meiduo/MainActivity.java
+++ app/src/main/java/com/meiduo/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
